93a3dfceb56622c43c7dc2b1bff7b1b1ac40e2ab
Afforess <afforess@gmail.com>
2012-07-02 20:31:40 -0400

    Engine Profiling

--- src/main/java/org/spout/engine/entity/SpoutEntity.java
+++ src/main/java/org/spout/engine/entity/SpoutEntity.java
@@ -55,6 +55,7 @@ import org.spout.api.model.Model;
 import org.spout.api.player.Player;
 import org.spout.api.tickable.Tickable;
 import org.spout.api.util.OutwardIterator;
+import org.spout.api.util.Profiler;
 import org.spout.engine.SpoutConfiguration;
 import org.spout.engine.SpoutEngine;
 import org.spout.engine.protocol.SpoutSession;
@@ -144,6 +145,7 @@ public class SpoutEntity extends Tickable implements Entity {
 
 	@Override
 	public void onTick(float dt) {
+		Profiler.start("tick entity session");
 		Controller cont = controllerLive.get();
 		//Pulse all player messages here, so they can interact with the entities position safely
 		if (cont instanceof PlayerController) {
@@ -154,6 +156,7 @@ public class SpoutEntity extends Tickable implements Entity {
 		}
 
 		//Tick the controller
+		Profiler.startAndStop("tick entity controller");
 		if (cont != null) {
 			//Sanity check
 			if (cont.getParent() != this) {
@@ -179,11 +182,13 @@ public class SpoutEntity extends Tickable implements Entity {
 		 * Copy over live chunk and entity manager values if this entity is valid. Transform copying is handled in
 		 * resolve (Tick Stage 2Pre).
 		 */
+		Profiler.startAndStop("tick entity chunk");
 		if (!isDead() && getPosition() != null && getWorld() != null) {
 			//Note: if the chunk is null, this effectively kills the entity (since dead: {chunkLive.get() == null})
 			chunkLive.set(getWorld().getChunkFromBlock(transform.getPosition(), LoadOption.NO_LOAD));
 			entityManagerLive.set(((SpoutRegion)getRegion()).getEntityManager());
 		}
+		Profiler.stop();
 	}
 
 	/**
